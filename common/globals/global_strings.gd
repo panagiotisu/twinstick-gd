2a7ed9f0456120a7766104f91412bebbf006c017
panagos <panosgeo11@gmail.com>
2026-02-18 19:24:59 +0200

    Add GlobalStrings Autoload

---
+++ common/globals/global_strings.gd
@@ -0,0 +1,15 @@
+extends Node
+
+class InputNames:
+	const MOVE_UP:    StringName = &"move_up"
+	const MOVE_DOWN:  StringName = &"move_down"
+	const MOVE_LEFT:  StringName = &"move_left"
+	const MOVE_RIGHT: StringName = &"move_right"
+	const ATTACK:     StringName = &"attack"
+	const RELOAD:     StringName = &"reload"
+	const DASH:       StringName = &"dash"
+	const GAME_PAUSE: StringName = &"game_pause"
+
+class ExportGroupNames:
+	const COMPONENTS: String = "Components"
+	const PROPERTIES: String = "Properties"
